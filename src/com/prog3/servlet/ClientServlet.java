791ca7a2822ad8ae38d8a298e423f692ee619469
Vittorio Fones <vittorio.fones@gmail.com>
2019-09-10 17:04:59 +0200

    First user and admin screen. (index.jsp)

--- src/com/prog3/servlet/ClientServlet.java
+++ src/com/prog3/servlet/ClientServlet.java
@@ -1,7 +1,7 @@
 package com.prog3.servlet;
 
 import com.prog3.hibernate.dao.ProductDao;
-import com.prog3.hibernate.model.Product;
+import com.prog3.hibernate.ormbean.ProductBean;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,24 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The type Client servlet.
+ */
 public class ClientServlet extends HttpServlet{
   private static final long serialVersionUID = 1L;
-
-  public ClientServlet() {
-    super();
-  }
-
-  private void doClientServlet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    ProductDao prod = new ProductDao();
-    List<Product> prod_list = prod.getAll();
-    req.setAttribute("list", prod_list);
-
-    String message = "Select drink...";
-    req.setAttribute("msg", message);
-
-    RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/client/index.jsp");
-    rd.forward(req, resp);
-  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -36,8 +23,36 @@ public class ClientServlet extends HttpServlet{
   }
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    doGet(request, response);
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doGet(req, resp);
+  }
+
+  /**
+   * Do client servlet.
+   *
+   * @param req  the req
+   * @param resp the resp
+   * @throws ServletException the servlet exception
+   * @throws IOException      the io exception
+   */
+  public void doClientServlet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    ProductDao prod = new ProductDao();
+    List<ProductBean> prod_list = prod.query("from ProductBean order by prod_id");
+
+    req.setAttribute("list", prod_list);
+
+    String msg = (String) req.getAttribute("msg");
+    if(msg == null)
+      msg = "Select drink...";
+    req.setAttribute("msg", msg);
+
+    String coins = (String) req.getAttribute("coins");
+    if(coins == null)
+      coins = "0.0";
+    req.setAttribute("coins", coins);
+
+    RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/client/index.jsp");
+    rd.forward(req, resp);
   }
 }
