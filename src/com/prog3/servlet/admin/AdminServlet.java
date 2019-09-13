4cff24f59fd43ccf443e38ee330fde5a87d2ea54
Vittorio Fones <vittorio.fones@gmail.com>
2019-09-13 22:29:34 +0200

    Limited AdminServlet, using control

--- src/com/prog3/servlet/admin/AdminServlet.java
+++ src/com/prog3/servlet/admin/AdminServlet.java
@@ -1,39 +1,28 @@
 package com.prog3.servlet.admin;
 
-import com.prog3.db.dao.GenericDao;
-import com.prog3.db.ormbean.Product;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class AdminServlet extends HttpServlet {
-
   protected void doAdmin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    //String loggedUser =  (String) req.getAttribute("loggedUser");
     RequestDispatcher rd = null;
-    String filtered = (String) req.getAttribute("loggedUser");
+    //if(loggedUser != null) {
 
-    if (filtered != null) {
-      GenericDao<Product> productDao = new GenericDao<Product>("from Product order by prod_id");
-      List<Product> prod_list = productDao.getAll();
-      req.setAttribute("list", prod_list);
+    AdminControl.adminControl(req, resp);
 
-      System.out.println("Succefful loginnn signinservlet filteredd");
-      rd = this.getServletContext().getRequestDispatcher("/admin/index.jsp");
+    rd = req.getRequestDispatcher("/admin/index.jsp");
 
+    /*}
+    else {
+      String err = "?err=\"true\"";
+      rd = req.getRequestDispatcher("/client"+err);
+    }*/
 
-
-
-    } else {
-      req.setAttribute("alert", "show");
-      rd = req.getRequestDispatcher("/client");
-      rd.forward(req, resp);
-    }
     rd.forward(req, resp);
   }
 
